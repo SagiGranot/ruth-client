c9982e8ef49472fb3581fde7b19b03fc4ece3358
Noa Eylon <noaeylon2@gmail.com>
2020-03-28 14:15:27 +0300

    add popup fields

--- src/configs/deployLayer.js
+++ src/configs/deployLayer.js
@@ -1,37 +1,46 @@
-import {Marker} from '../resources/markers';
+import { Marker } from "../resources/markers";
 
 export const deployLayerOpt = {
-    title: 'deployments',
-    objectIdField: 'OBJECTID',
-    geometryType: 'point',
-    spatialReference: { wkid: 4326 },
-    fields: [
-      {
-        name: 'deployType',
-        alias: 'TYPE',
-        type: 'string'
-      },
-      {
-        name: 'additionalInfo',
-        alias: 'INFO',
-        type: 'string'
-      },
-      {
-        name: 'totalAmount',
-        alias: 'AMOUNT',
-        type: 'string'
-      },
-      {
-        name: 'deployId',
-        alias: 'ID',
-        type: 'string'
-      }
-    ],
-    renderer: {
-      type: "unique-value",
-      field: "deployType",
-      uniqueValueInfos: [Marker.FriendlyMarker,Marker.EnemyMarker, Marker.meMarker]
+  title: "deployments",
+  objectIdField: "OBJECTID",
+  geometryType: "point",
+  spatialReference: { wkid: 4326 },
+  fields: [
+    {
+      name: "deployType",
+      alias: "TYPE",
+      type: "string"
     },
-    labelingInfo: [ Marker.amountLabel ],
-    popupTemplate: Marker.popupTemplate
-  }
+    {
+      name: "additionalInfo",
+      alias: "INFO",
+      type: "string"
+    },
+    {
+      name: "totalAmount",
+      alias: "AMOUNT",
+      type: "string"
+    },
+    {
+      name: "deployId",
+      alias: "ID",
+      type: "string"
+    },
+    {
+      name: "reportingUserId",
+      alias: "Reporting User",
+      type: "string"
+    }
+  ],
+  renderer: {
+    type: "unique-value",
+    field: "deployType",
+    uniqueValueInfos: [
+      Marker.FriendlyMarker,
+      Marker.EnemyMarker,
+      Marker.meMarker
+    ]
+  },
+  labelingInfo: [Marker.amountLabel],
+  popupTemplate: Marker.popupTemplate
+};
