38cfd02da59b16237ff613889d7f433cb381a43c
Tomer <tomerl101@gmail.com>
2020-05-05 22:00:16 +0300

    imporve map

--- src/markers/object.js
+++ src/markers/object.js
@@ -1,18 +1,38 @@
 const objectMarker = {
   BuildingMarker: {
-    value: "Building",
+    value: 'Building',
     symbol: {
-      type: "polygon-3d",
+      type: 'polygon-3d',
       symbolLayers: [
         {
-          type: "extrude",
+          type: 'extrude',
           size: 0, // extrude by 10 meters
           material: {
             color: [255, 255, 255, 0.8],
           },
           edges: {
-            type: "solid",
-            size: "3px",
+            type: 'solid',
+            size: '3px',
+            color: [82, 82, 122, 0.9],
+          },
+        },
+      ],
+    },
+  },
+  SuspiciousMarker: {
+    value: 'Suspicious',
+    symbol: {
+      type: 'polygon-3d',
+      symbolLayers: [
+        {
+          type: 'extrude',
+          size: 0, // extrude by 10 meters
+          material: {
+            color: [255, 0, 0, 0.8],
+          },
+          edges: {
+            type: 'solid',
+            size: '3px',
             color: [82, 82, 122, 0.9],
           },
         },
