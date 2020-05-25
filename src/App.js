cc3f19dc06281ecbfc0ed27b08e7a387092681fb
Noa Eylon <noaeylon2@gmail.com>
2020-05-25 22:38:24 +0300

    add weather widget

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React, { useEffect } from 'react';
-import socketIOClient from 'socket.io-client';
-import { Header } from './components/Header';
-import './resources/style/Style.css';
-import { Notification } from './components/Notification';
-import { MapSceneView } from './components/MapSceneView';
-import { ToastProvider } from 'react-toast-notifications';
-import { CustomToast } from './components/Toast';
+import React, { useEffect } from "react";
+import socketIOClient from "socket.io-client";
+import { Header } from "./components/Header";
+import "./resources/style/Style.css";
+import { Notification } from "./components/Notification";
+import { MapSceneView } from "./components/MapSceneView";
+import { ToastProvider } from "react-toast-notifications";
+import { CustomToast } from "./components/Toast";
 
 const App = () => {
-  const socketio = socketIOClient('https://fierce-everglades-47378.herokuapp.com');
+  const socketio = socketIOClient("http://localhost:8080");
   const params = new URLSearchParams(window.location.search);
-  const userId = params.get('userId');
+  const userId = params.get("userId");
 
   return (
     <div className="App">
       <Header />
-      <ToastProvider components={{ Toast: CustomToast }} placement="bottom-right">
+      <ToastProvider
+        components={{ Toast: CustomToast }}
+        placement="bottom-right"
+      >
         <Notification socketio={socketio} userId={userId} />
         <MapSceneView socketio={socketio} userId={userId} />
         <footer />
