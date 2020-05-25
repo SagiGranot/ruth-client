ac308bebf4d03a951db916ac764b13338653f10c
Tomer <tomerl101@gmail.com>
2020-05-25 20:31:23 +0300

    add colors to logs

--- src/components/DeltaLogs.js
+++ src/components/DeltaLogs.js
@@ -1,5 +1,7 @@
-import React, { Component } from "react";
-import { loadModules } from "esri-loader";
+import React, { Component } from 'react';
+import { loadModules } from 'esri-loader';
+
+const COLORS = { ENEMY_CLOSER: 'yellow', ENEMY_SURROUNDING: 'red', ASSIST_FRIENDLY: 'blue' };
 
 export class DeltaLogs extends React.Component {
   constructor() {
@@ -8,27 +10,27 @@ export class DeltaLogs extends React.Component {
   }
 
   componentDidMount() {
-    loadModules(["esri/widgets/Expand"], {
+    loadModules(['esri/widgets/Expand'], {
       css: false,
     }).then(async ([Expand]) => {
       const expand = new Expand({
-        expandIconClass: "esri-icon-key",
-        expandTooltip: "Expand DeltaLogs widget",
+        expandIconClass: 'esri-icon-duplicate',
+        expandTooltip: 'Expand DeltaLogs widget',
         view: this.props.view,
-        content: document.getElementById("logsContent"),
+        content: document.getElementById('logsContent'),
         expanded: false,
       });
-      this.props.view.ui.add(expand, "top-right");
+      this.props.view.ui.add(expand, 'top-right');
 
-      this.props.socketio.on("NOTIFICATION", this.addLogsToWidget);
-      this.props.socketio.on("ENEMY_CLOSER_" + this.props.userId, (log) =>
-        this.addLogsToWidget(log, "ENEMY_CLOSER_")
+      this.props.socketio.on('NOTIFICATION', this.addLogsToWidget);
+      this.props.socketio.on('ENEMY_CLOSER_' + this.props.userId, (log) =>
+        this.addLogsToWidget(log, 'ENEMY_CLOSER')
       );
-      this.props.socketio.on("ENEMY_SURROUNDING_" + this.props.userId, (log) =>
-        this.addLogsToWidget(log, "ENEMY_SURROUNDING_")
+      this.props.socketio.on('ENEMY_SURROUNDING_' + this.props.userId, (log) =>
+        this.addLogsToWidget(log, 'ENEMY_SURROUNDING')
       );
-      this.props.socketio.on("ASSIST_FRIENDLY_" + this.props.userId, (log) =>
-        this.addLogsToWidget(log, "ASSIST_FRIENDLY_")
+      this.props.socketio.on('ASSIST_FRIENDLY_' + this.props.userId, (log) =>
+        this.addLogsToWidget(log, 'ASSIST_FRIENDLY')
       );
 
       this.deltasLogs = this.props.deltas;
@@ -37,41 +39,44 @@ export class DeltaLogs extends React.Component {
   }
 
   addLogsToWidget(log, type) {
-    console.log(type);
-    console.log(log);
-    console.log("----------");
+    this.deltasLogs.unshift({ message: type, timestamp: log.timestamp });
+    this.forceUpdate();
   }
 
   render() {
     return (
       <div
         style={{
-          width: "400px",
-          height: "400px",
-          padding: "5px",
-          margin: "0",
-          backgroundColor: "black",
-          opacity: "70%",
+          width: '400px',
+          height: '400px',
+          padding: '5px',
+          margin: '0',
+          backgroundColor: 'black',
+          opacity: '70%',
         }}
         id="logsContent"
       >
-        <h3 style={{ padding: "5px" }}>Delta Recent Logs</h3>
+        <h3 style={{ padding: '5px' }}>Delta Recent Logs</h3>
         <div
           style={{
-            padding: "5px",
-            overflowY: "scroll",
-            width: "100%",
-            height: "300px",
+            padding: '5px',
+            overflowY: 'scroll',
+            width: '100%',
+            height: '300px',
           }}
         >
           {this.deltasLogs.map((value, index) => {
             let time = new Date(value.timestamp);
             return (
-              <div style={{ margin: "4px" }} id={value._id}>
-                <h4 key={index}>{value.message}</h4>
-                <h5>
-                  {time.toDateString()} --- {time.getUTCHours()}:
-                  {time.getMinutes()}
+              <div
+                style={{ margin: '4px', marginBottom: '20px', color: COLORS[value.message] }}
+                id={value._id}
+              >
+                <h4 style={{ margin: '0' }} key={index}>
+                  {value.message}
+                </h4>
+                <h5 style={{ margin: '0' }}>
+                  {time.toDateString()} --- {time.getUTCHours()}:{time.getMinutes()}
                 </h5>
               </div>
             );
