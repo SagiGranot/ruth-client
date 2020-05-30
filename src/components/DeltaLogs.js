10f0dcc22b13432868b39a8b69c7b1033cdee61b
Noa Eylon <noaeylon2@gmail.com>
2020-05-30 19:20:07 +0300

    fixed delta and notification ui

--- src/components/DeltaLogs.js
+++ src/components/DeltaLogs.js
@@ -1,7 +1,12 @@
-import React, { Component } from 'react';
-import { loadModules } from 'esri-loader';
+import React, { Component } from "react";
+import { loadModules } from "esri-loader";
 
-const COLORS = { ENEMY_CLOSER: 'yellow', ENEMY_SURROUNDING: 'red', ASSIST_FRIENDLY: 'blue' };
+const COLORS = {
+  ENEMY_CLOSER: "#f5cd36",
+  ENEMY_SURROUNDING: "#f50e6e",
+  ASSIST_FRIENDLY: "#44f47b",
+  BLUE: "#4466f4",
+};
 
 export class DeltaLogs extends React.Component {
   constructor() {
@@ -10,27 +15,27 @@ export class DeltaLogs extends React.Component {
   }
 
   componentDidMount() {
-    loadModules(['esri/widgets/Expand'], {
+    loadModules(["esri/widgets/Expand"], {
       css: false,
     }).then(async ([Expand]) => {
       const expand = new Expand({
-        expandIconClass: 'esri-icon-duplicate',
-        expandTooltip: 'Expand DeltaLogs widget',
+        expandIconClass: "esri-icon-duplicate",
+        expandTooltip: "Expand DeltaLogs widget",
         view: this.props.view,
-        content: document.getElementById('logsContent'),
+        content: document.getElementById("logsContent"),
         expanded: false,
       });
-      this.props.view.ui.add(expand, 'top-right');
+      this.props.view.ui.add(expand, "top-right");
 
-      this.props.socketio.on('NOTIFICATION', this.addLogsToWidget);
-      this.props.socketio.on('ENEMY_CLOSER_' + this.props.userId, (log) =>
-        this.addLogsToWidget(log, 'ENEMY_CLOSER')
+      this.props.socketio.on("NOTIFICATION", this.addLogsToWidget);
+      this.props.socketio.on("ENEMY_CLOSER_" + this.props.userId, (log) =>
+        this.addLogsToWidget(log, "ENEMY_CLOSER")
       );
-      this.props.socketio.on('ENEMY_SURROUNDING_' + this.props.userId, (log) =>
-        this.addLogsToWidget(log, 'ENEMY_SURROUNDING')
+      this.props.socketio.on("ENEMY_SURROUNDING_" + this.props.userId, (log) =>
+        this.addLogsToWidget(log, "ENEMY_SURROUNDING")
       );
-      this.props.socketio.on('ASSIST_FRIENDLY_' + this.props.userId, (log) =>
-        this.addLogsToWidget(log, 'ASSIST_FRIENDLY')
+      this.props.socketio.on("ASSIST_FRIENDLY_" + this.props.userId, (log) =>
+        this.addLogsToWidget(log, "ASSIST_FRIENDLY")
       );
 
       this.deltasLogs = this.props.deltas;
@@ -47,35 +52,55 @@ export class DeltaLogs extends React.Component {
     return (
       <div
         style={{
-          width: '400px',
-          height: '400px',
-          padding: '5px',
-          margin: '0',
-          backgroundColor: 'black',
-          opacity: '70%',
+          width: "400px",
+          height: "400px",
+          padding: "5px",
+          margin: "0",
+          backgroundColor: "black",
+          opacity: "70%",
         }}
         id="logsContent"
       >
-        <h3 style={{ padding: '5px' }}>Delta Recent Logs</h3>
+        <h2 style={{ textAlign: "center" }}>Delta Recent Logs</h2>
         <div
           style={{
-            padding: '5px',
-            overflowY: 'scroll',
-            width: '100%',
-            height: '300px',
+            padding: "5px",
+            overflowY: "scroll",
+            width: "100%",
+            height: "300px",
           }}
         >
           {this.deltasLogs.map((value, index) => {
             let time = new Date(value.timestamp);
             return (
               <div
-                style={{ margin: '4px', marginBottom: '20px', color: COLORS[value.message] }}
+                style={{
+                  //margin: "4px",
+                  //marginBottom: "20px",
+                  // color: COLORS[value.message],
+                  display: "flex",
+                  margin: "4px 4px 20px",
+                  justifyContent: "space-evenly",
+                  alignItems: "center",
+                }}
                 id={value._id}
                 key={index}
               >
-                <h4 style={{ margin: '0' }}>{value.message}</h4>
-                <h5 style={{ margin: '0' }}>
-                  {time.toDateString()} --- {time.getUTCHours()}:{time.getMinutes()}
+                <div
+                  style={{
+                    margin: "0px",
+                    backgroundColor: COLORS[value.message],
+                    width: "10px",
+                    height: "10px",
+                    borderRadius: "50px",
+                    boxShadow: "0px 0px 7px" + COLORS[value.message],
+                  }}
+                ></div>
+
+                <h4 style={{ margin: "0" }}>{value.message}</h4>
+                <h5 style={{ margin: "0" }}>
+                  {time.toDateString()} --- {time.getUTCHours()}:
+                  {time.getMinutes()}
                 </h5>
               </div>
             );
--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,15 +1,16 @@
-import { useEffect } from 'react';
-import { useToasts } from 'react-toast-notifications';
+import { useEffect } from "react";
+import { useToasts } from "react-toast-notifications";
 
-export const Notification = ({ socketio,userId }) => {
+export const Notification = ({ socketio, userId }) => {
   const { addToast } = useToasts();
 
   useEffect(() => {
     // socketio.on('NOTIFICATION', showWarningToast);
-    socketio.on('NOTIFICATION', showWarningToast);
-    socketio.on('ENEMY_CLOSER_' + userId, showEnemyCloser);
-    socketio.on('ENEMY_SURROUNDING_' + userId, showEnemySurrounding);
-    socketio.on('ASSIST_FRIENDLY_'+ userId, showAssist);
+    socketio.on("NOTIFICATION", showWarningToast);
+    socketio.on("ENEMY_CLOSER_" + userId, showEnemyCloser);
+    socketio.on("ENEMY_SURROUNDING_" + userId, showEnemySurrounding);
+    socketio.on("SUSPECT-BUILDING", showSuspectBuilding);
+    socketio.on("ASSIST_FRIENDLY_" + userId, showAssist);
   }, []);
 
   const showWarningToast = async ({ content, type }) => {
@@ -17,17 +18,25 @@ export const Notification = ({ socketio,userId }) => {
   };
 
   const showEnemyCloser = async ({ content, type }) => {
-    addToast('ENEMY CLOSER!', { appearance: 'warning', autoDismiss: true });
+    addToast("ENEMY CLOSER!", { appearance: "warning", autoDismiss: true });
   };
 
   const showEnemySurrounding = async ({ content, type }) => {
     //
-    addToast('ENEMY SURROUNDING!', { appearance: 'error', autoDismiss: true });
+    addToast("ENEMY SURROUNDING!", { appearance: "error", autoDismiss: true });
+  };
+
+  const showSuspectBuilding = async ({ content, type }) => {
+    //
+    addToast("SUSPECT BUILDING", { appearance: "info", autoDismiss: true });
   };
 
   const showAssist = async ({ content, type }) => {
     //
-    addToast('ASSIST FRIENDLY ALPHA', { appearance: 'info', autoDismiss: true });
+    addToast("ASSIST FRIENDLY ALPHA", {
+      appearance: "success",
+      autoDismiss: true,
+    });
   };
   return null;
 };
